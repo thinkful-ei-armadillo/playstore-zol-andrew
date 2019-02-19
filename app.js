b0112300aff6a76df56141df691408513d1fdc45
andrewbituin <fandrewbituin@gmail.com>
2019-02-19 14:00:53 -0800

    finished functionality

--- app.js
+++ app.js
@@ -1,48 +1,48 @@
-'use strict';
-const express = require('express');
-const morgan = require('morgan');
+"use strict";
+const express = require("express");
+const morgan = require("morgan");
 
 const app = express();
 
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 
-const playstore = require('./playstore'); 
+const playstore = require("./playstore");
 
-app.get('/apps', (req, res) => {
-//   if(!req.query){
-//     res.send(playstore);
-//   }
+app.get("/apps", (req, res) => {
+  const { sort, genres } = req.query;
 
-  const { sort } = req.query;
-    
-  if(sort) {
-    if(!['rating', 'app'].includes(sort)) {
-      return res
-        .status(400)
-        .send('Sort must be one of rating or app');
+  if (sort) {
+    if (!["Rating", "App"].includes(sort)) {
+      return res.status(400).send("Sort must be one of rating or app");
     }
   }
-
-  //   let results = books
-  //     .filter(book => 
-  //       book
-  //         .title
-  //         .toLowerCase()
-  //         .includes(search.toLowerCase()));
-
+  if (genres) {
+    if (
+      !["Action", "Puzzle", "Strategy", "Casual", "Arcade", "Card"].includes(
+        genres
+      )
+    ) {
+      return res.status(400).send("Must be an acceptable genre.");
+    }
+  }
   let results = playstore;
-  res.send(results);
-  if(sort) {
-    results
-      .sort((a, b) => {
-        return a[sort] > b[sort] ? 1 : a[sort] < b[sort] ? -1 : 0;
-      }); 
-  }  
-
-  res
-    .json(results);
+  if (sort) {
+    results.sort((a, b) => {
+      return a[sort] > b[sort] ? 1 : a[sort] < b[sort] ? -1 : 0;
+    });
+    res.json(results);
+  }
+  if (genres) {
+    let newResults = results.filter(app => {
+      let string = app["Genres"];
+      if (string.includes(genres)) {
+        return app;
+      }
+    });
+    res.json(newResults);
+  }
 });
 
 app.listen(8000, () => {
-  console.log('listening on port 8000');
+  console.log("listening on port 8000");
 });
